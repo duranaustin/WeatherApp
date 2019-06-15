77073087352fd82ac57a4c737c980f264e4548f7
EthanDarling8 <edarling90@gmail.com>
2019-06-14 20:20:43 -0600

    Added a way to parse the deeper parts of the JSON sections.

--- src/weatherapp/WeatherEngine.java
+++ src/weatherapp/WeatherEngine.java
@@ -1,16 +1,11 @@
 package weatherapp;
 
-import javax.swing.text.Keymap;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Brains of WeatherApp parses API data and provides fields for WeatherUI.java
@@ -20,11 +15,11 @@ public class WeatherEngine {
     private String getRequest = null;
     private String getResponse = null;
     private String currentDay = null;
-    private String currentFarenheitDegree = null;
-    private String currentWeatherStatus = null;
+    private String currentFarenheitDegree;
+    private String currentWeatherStatus;
     private String currentFarenheitDegreeHigh = null;
     private String currentFarenheitDegreeLow = null;
-    private String currentCity = null;
+    private String currentCity;
     private String currentState = null;
     private WeatherDay monday;
     private WeatherDay tuesday;
@@ -35,6 +30,8 @@ public class WeatherEngine {
     private WeatherDay sunday;
     private WeatherAPI api;
     public HashMap<String, Object> map;
+    private String[] parts1;
+    private String[] parts2;
 
     /**
      * Upon object creation of WeatherEngine a WeatherAPI object is created  in order to populate WeatherEngine fields/instance variables
@@ -45,6 +42,12 @@ public class WeatherEngine {
         setKeyMap();
         monday = getMonday();
         //TODO set all fields by parsing json file from src/resources/myfile.json
+
+        // Testing mapParser method
+        System.out.println();
+        mapParser("name");
+        mapParser("main");
+        mapParser("weather");
     }
 
     private void setKeyMap(){
@@ -57,20 +60,55 @@ public class WeatherEngine {
             this.map = mapper.readValue(json, new TypeReference<Map<String, Object>>(){});
 
             //Print JSON output
-            System.out.println(this.map);
-            System.out.println(this.map.get("name"));
-            System.out.println(this.map.get("main"));
-        }
-        catch (JsonGenerationException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
+//            System.out.println(this.map);
+//            System.out.println(this.map.get("name"));
+//            System.out.println(this.map.get("main"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Parses the string retrieved from the setKeyMap method map. Once the string is
+     * retrieved it then goes through the jsonStringSplitter method to get split into
+     * it's individual items.
+     * TODO There may be another more efficient/simple way to get deeper into the JSON sections.
+     * @param mapString The String that is used to look up a particular map value.
+     */
+    private void mapParser(String mapString) {
+        String ss = this.map.get(mapString).toString();
+        if (mapString.equals("name")) {
+            currentCity = this.map.get(mapString).toString();
+            System.out.println(currentCity);
+        }
+        else if (mapString.equals("main")) {
+            jsonStringSplitter(ss);
+            currentFarenheitDegree = parts2[1] ;
+            System.out.println(currentFarenheitDegree);
+        }
+        else if (mapString.equals("weather")) {
+            jsonStringSplitter(ss);
+            currentWeatherStatus = parts2[5] ;
+            System.out.println(currentWeatherStatus);
+        }
+    }
 
+    /**
+     * Splits the string that is retrieved from mapParser method and puts them in a String[].
+     *
+     * @param ss The String that is going to be split.
+     */
+    private void jsonStringSplitter(String ss) {
+        StringBuilder sb = new StringBuilder();
+        parts1 = ss.split(", ");
+        for (String s : parts1) {
+            sb.append(s).append("=");
+        }
+        sb.deleteCharAt(sb.length() - 2);
+        String t = sb.toString();
+
+        parts2 = t.split("=");
+    }
 
     private WeatherDay getMonday() {
         //TODO use api object for weather info
